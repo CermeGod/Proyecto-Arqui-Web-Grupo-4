95bbe4ccb629f89d4659c6632529e8a1712c034e
Axelib <137766888+Axelib@users.noreply.github.com>
2026-04-26 10:41:06 -0500

    Delete Inmovision directory
    Eliminacion de carpetas innecesarias

--- Inmovision/src/main/java/pe/edu/upc/inmovision/repositories/IPropiedadRepository.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.inmovision.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.inmovision.entities.Propiedades;
-@Repository
-public interface IPropiedadRepository extends JpaRepository<Propiedades,Integer> {
-}
