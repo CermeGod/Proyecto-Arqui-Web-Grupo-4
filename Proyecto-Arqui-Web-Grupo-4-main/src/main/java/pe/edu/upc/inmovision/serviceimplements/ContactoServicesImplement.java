c6b3a4721055e1644b6f5b0142eabe83764ce98b
Sebastian Arotinco <sebastianarotinco6@gmail.com>
2026-04-29 02:53:58 -0500

    Realizar parte del trabajo con controllers incluido2

---
+++ Proyecto-Arqui-Web-Grupo-4-main/src/main/java/pe/edu/upc/inmovision/serviceimplements/ContactoServicesImplement.java
@@ -0,0 +1,41 @@
+package pe.edu.upc.inmovision.serviceimplements;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import pe.edu.upc.inmovision.entities.Contacto;
+import pe.edu.upc.inmovision.repositories.IContactoRepository;
+import pe.edu.upc.inmovision.serviceinterfaces.IContactoService;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ContactoServicesImplement implements IContactoService {
+    @Autowired
+    private IContactoRepository cR;
+
+    @Override
+    public Contacto insertar(Contacto contacto) {
+        return cR.save(contacto);
+    }
+
+    @Override
+    public List<Contacto> listar() {
+        return cR.findAll();
+    }
+
+    @Override
+    public Optional<Contacto> buscarPorId(int id) {
+        return cR.findById(id);
+    }
+
+    @Override
+    public void delete(int id) {
+        cR.deleteById(id);
+
+    }
+
+    @Override
+    public void update(Contacto contacto) {
+        cR.save(contacto);
+
+    }
+}
