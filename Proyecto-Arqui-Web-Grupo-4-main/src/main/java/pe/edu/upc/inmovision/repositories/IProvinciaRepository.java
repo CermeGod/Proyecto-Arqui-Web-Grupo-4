29ac252f7703161af6215cd939d3919f27a0f27f
Axelib <axelstephano24@gmail.com>
2026-05-10 15:17:37 -0500

    Eliminando carpeta duplicada del repo

--- Proyecto-Arqui-Web-Grupo-4-main/src/main/java/pe/edu/upc/inmovision/repositories/IProvinciaRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.inmovision.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.inmovision.entities.Provincia;
-
-@Repository
-public interface IProvinciaRepository  extends JpaRepository<Provincia,Integer> {
-}
--- Proyecto-Arqui-Web-Grupo-4-main/src/main/java/pe/edu/upc/inmovision/repositories/IRecomendacionesRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.inmovision.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.inmovision.entities.Recomendaciones;
-
-@Repository
-public interface IRecomendacionesRepository extends JpaRepository<Recomendaciones, Integer> {
-}
